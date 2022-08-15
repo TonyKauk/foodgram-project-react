f596341bcdace9075a406a47d670935c5062ddbe
Anton Milov <milovanton@gmail.com>
2022-08-15 22:52:52 +0300

    recipes app added

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipes.apps.RecipesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
