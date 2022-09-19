217c2d56480b5919ebd287e52e40e81cb61d701c
Anton Milov <milovanton@gmail.com>
2022-09-19 21:24:54 +0300

    Admin zone ready

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
 
 from .models import Recipe, Tag, Ingredient, IngredientAmount
 from .models import FollowAuthor, FavoriteRecipe, Cart
-# from users.models import User
 
 
 class Admin(admin.ModelAdmin):
@@ -18,21 +16,26 @@ class Admin(admin.ModelAdmin):
         return True
 
 
-admin.site.register(Recipe, Admin)
-admin.site.register(Tag, Admin)
-admin.site.register(Ingredient, Admin)
-# admin.site.register(IngredientAmount, Admin)
-# admin.site.register(FollowAuthor, Admin)
-# admin.site.register(FavoriteRecipe, Admin)
-# admin.site.register(Cart, Admin)
+class RecipeAdmin(Admin):
+    list_display = ('name', 'author', 'times_favorited')
+    list_filter = ('name', 'author', 'tags')
+
+    def times_favorited(self, object):
+        result = object.added_to_favorite.count()
+        return result
+
+
+class IngredientAdmin(Admin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
 
 
 admin.site.unregister(User)
-
-
-# @admin.register(User)
-# class CustomUserAdmin(UserAdmin):
-#     pass
-
-
 admin.site.register(User, Admin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, Admin)
+admin.site.register(IngredientAmount, Admin)
+admin.site.register(FollowAuthor, Admin)
+admin.site.register(FavoriteRecipe, Admin)
+admin.site.register(Cart, Admin)
